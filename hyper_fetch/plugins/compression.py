1ff821f5326e9c0ca3235076a2ea9eaf615f51db
Masroor Ehsan <masroore@gmail.com>
2025-02-23 12:44:57 +0600

    Add dynamic support for Brotli and Zstd compression

--- hyper_fetch/plugins/compression.py
+++ hyper_fetch/plugins/compression.py
@@ -1,3 +1,16 @@
+# optional dependencies
+try:
+    import brotli
+except ImportError:  # pragma: no cover
+    try:
+        import brotlicffi as brotli
+    except ImportError:
+        brotli = None
+try:
+    import zstandard
+except ImportError:  # pragma: no cover
+    zstandard = None  # type: ignore
+
 from hyper_fetch.types import Plugin, DownloadRequest, DownloadResult
 
 
@@ -10,11 +23,25 @@ class CompressionPlugin(Plugin):
     async def pre_request(self, request: DownloadRequest) -> DownloadRequest:
         if request.headers is None:
             request.headers = {}
-        request.headers["Accept-Encoding"] = "gzip, deflate"
+        request.headers["Accept-Encoding"] = ", ".join(SUPPORTED_ENCODINGS)
         return request
 
     async def post_response(self, result: DownloadResult) -> DownloadResult:
-        return result
+        pass
 
     async def cleanup(self) -> None:
         pass
+
+
+SUPPORTED_ENCODINGS = [
+    "identity",
+    "gzip",
+    "deflate",
+    "br",
+    "zstd",
+]
+
+if brotli is None:
+    SUPPORTED_ENCODINGS.pop("br")  # pragma: no cover
+if zstandard is None:
+    SUPPORTED_ENCODINGS.pop("zstd")  # pragma: no cover
